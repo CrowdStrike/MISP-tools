d1e9ae2309f9089dccc978f4652802398a1fc20e
Joshua Hiller <joshua.hiller@crowdstrike.com>
2022-08-09 10:12:30 -0400

    Adversary enumerator

---
+++ cs_misp_import/adversary.py
@@ -0,0 +1,19 @@
+from enum import Enum
+
+class Adversary(Enum):
+    """Adversary enumerator."""
+
+    BEAR = "Russian Federation"
+    BUFFALO = "Vietnam"
+    CHOLLIMA = "North Korea (DPRK)"
+    CRANE = "Republic of Korea (ROK)"
+    JACKAL = "Hacktivist"
+    KITTEN = "Iran"
+    LEOPARD = "Pakistan"
+    LYNX = "Georgia"
+    OCELOT = "Columbia"
+    PANDA = "People's Republic of China"
+    SPIDER = "eCrime"
+    TIGER = "India"
+    WOLF = "Turkey"
+    
