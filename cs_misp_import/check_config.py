6b1fa5c5cca920908ea4296f9631c4679f4884c5
Joshua Hiller <joshua.hiller@crowdstrike.com>
2022-10-04 20:04:12 -0400

    Linting and optimization refactor

--- cs_misp_import/check_config.py
+++ cs_misp_import/check_config.py
@@ -4,12 +4,12 @@ from logging import basicConfig, getLogger, INFO, Logger, DEBUG
 from configparser import ConfigParser
 from datetime import datetime
 from falconpy import BaseURL, Intel
-
+from .helper import CONFIG_BANNER
 
 BOOL_KEYS = [
     "api_enable_ssl", "misp_enable_ssl", "tag_unknown_galaxy_maps", "taxonomic_kill-chain",
-    "taxonomic_information-security-data-source", "taxonomic_type", "taxonomic_iep", "taxonomic_iep2",
-    "taxonomic_iep2_version", "taxonomic_tlp", "taxonomic_workflow"
+    "taxonomic_information-security-data-source", "taxonomic_type", "taxonomic_iep",
+    "taxonomic_iep2", "taxonomic_iep2_version", "taxonomic_tlp", "taxonomic_workflow"
 ]
 
 REDACTED = ['client_id', 'client_secret', 'misp_auth_key']
@@ -71,7 +71,7 @@ class ConfigurationCheckResult:
 
 def config_logging(do_debug: bool = False):
     """Configure log formatting and return a Logger instance."""
-    log_format = '[%(asctime)s] %(levelname)-8s %(name)s (%(key)s) %(message)s'
+    log_format = '[%(asctime)s] %(levelname)-8s %(name)s  %(key)-42s  %(message)s'
     basicConfig(format=log_format)
     logger = getLogger("config_check")
     if do_debug:
@@ -92,26 +92,34 @@ def read_config_file(filename: str = "misp_import.ini"):
 
 def not_found():
     """Display the invalid INI file message and raise an error condition."""
-    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S,%f")[:-3]
-    raise SystemExit(f"[{now}] CRITICAL config_check (NOT FOUND) Unable to read INI file or parse configuration sections")
+    #raise SystemExit(
+    print(
+        f"[{cur_time()}] CRITICAL config_check  Unable to read or parse configuration file"
+        )
+    return []
 
 
 def is_valid_config(result: ConfigurationCheckResult):
     """Check for validity and display the result."""
     valid_config = False
-    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S,%f")[:-3]
+    sev_detail = "INFO     config_check"
     if result.valid:
-        print(f"[{now}] INFO     config_check (VALID) No configuration errors found {result.total_warnings()}")
+        print(
+            f"[{cur_time()}] {sev_detail}  No configuration errors found {result.total_warnings()}"
+            )
         valid_config = True
     else:
-        print(f"[{now}] INFO     config_check (INVALID) {result.total_errors()} {result.total_warnings()}")
+
+        print(
+            f"[{cur_time()}] {sev_detail}  {result.total_errors()} {result.total_warnings()}"
+            )
 
     return valid_config
 
 
-def invalid_value(rslt: ConfigurationCheckResult, item: str, check_val: str):
+def invalid_value(rslt: ConfigurationCheckResult, check_val: str):
     """Log an invalid value error."""
-    rslt.put(f"ERROR: Invalid value specified for '{item}' ({check_val})")
+    rslt.put(f"ERROR: Invalid value specified ({check_val})")
 
     return False
 
@@ -148,12 +156,14 @@ def valid_base(base: str):
 
 
 def failure(rslt: ConfigurationCheckResult, msg_str: str):
+    """Process a failure log."""
     rslt.put(msg_str)
 
     return False
 
 
 def warning(rslt: ConfigurationCheckResult, msg_str: str):
+    """Process a warning log."""
     rslt.put(msg_str)
 
     return True
@@ -189,86 +199,161 @@ def check_for_missing(rslt: ConfigurationCheckResult, keyz: dict):
     for check, check_val in keyz.items():
         rslt.extra = {"key": check, "section": None}
         if check_val == "WARNING":
-            warning(rslt, "WARNING: Missing configuration value, using default")
+            warning(rslt, "WARNING: Missing configuration parameter, using default")
         if check_val == "CRITICAL":
-            failure(rslt, "CRITICAL: Missing configuration value")
+            failure(rslt, "CRITICAL: Missing configuration parameter")
         if check_val == "ERROR":
-            failure(rslt, "ERROR: Missing configuration value")
+            failure(rslt, "ERROR: Missing configuration parameter")
 
 
-def validate_config(config_file: str = None, debugging: bool = False):
+def cur_time():
+    """Return the current time in human readable format."""
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S,%f")[:-3]
+
+
+def validate_crowdstrike_creds(c_key: str,
+                               c_val: str,
+                               keyz: dict,
+                               logg: ConfigurationCheckResult,
+                               auth: dict
+                               ):
+    """Validate CrowdStrike client credential and base_url parameters."""
+    if c_key == "client_id":  # 32 chars
+        auth["creds"][c_key] = c_val
+        keyz[c_key] = failure(logg,
+            "CRITICAL: CrowdStrike client ID is formatted incorrectly",
+            ) if len(c_val) != 32 else True
+    if c_key == "client_secret":  # 40 chars
+        auth["creds"][c_key] = c_val
+        keyz[c_key] = failure(logg,
+            "CRITICAL: CrowdStrike client secret is formatted incorrectly"
+            ) if len(c_val) != 40 else True
+    if c_key == "crowdstrike_url":
+        auth["base_url"] = c_val
+        keyz[c_key] = warning(logg,
+            "WARNING: Non-standard CrowdStrike URL specified"
+            ) if not valid_base(c_val) else True
+
+
+def validate_misp_creds(c_key: str, c_val: str, keyz: dict, logg: ConfigurationCheckResult):
+    """Validate MISP authorization key formatting."""
+    if c_key == "misp_auth_key":  # 40 chars
+        keyz[c_key] = failure(
+            logg, "CRITICAL: MISP authorization key is formatted incorrectly"
+            ) if len(c_val) != 40 else True
+
+
+def validate_ssl(c_key: str, c_val: str, keyz: dict, logg: ConfigurationCheckResult):
+    """Validate SSL encryption settings."""
+    if c_key == "api_enable_ssl":
+        keyz[c_key] = warning(
+            logg, "WARNING: SSL is disabled for CrowdStrike API requests"
+            ) if "f" in c_val.lower() else True
+    if c_key == "misp_enable_ssl":
+        keyz[c_key] = warning(
+            logg, "WARNING: SSL is disabled for MISP API requests"
+            ) if "f" in c_val.lower() else True
+
+
+def validate_start_times(c_key: str, c_val: str, keyz: dict, logg: ConfigurationCheckResult):
+    """Validate the default start times."""
+    if c_key == "init_reports_days_before":
+        keyz[c_key] = invalid(logg, c_val) if (0 > int(c_val) or int(c_val) > 365) else True
+    if c_key == "init_indicators_minutes_before":
+        keyz[c_key] = invalid(logg, c_val) if (0 > int(c_val) or int(c_val) > 20220) else True
+    if c_key == "init_actors_days_before":
+        keyz[c_key] = invalid(logg, c_val) if (0 > int(c_val) or int(c_val) > 730) else True
+
+
+def validate_booleans(c_key: str, c_val: str, keyz: dict, logg: ConfigurationCheckResult):
+    """Validate all boolean parameters."""
+    if c_key in BOOL_KEYS:
+        keyz[c_key] = invalid(logg, c_val) if not bool_str(c_val) else True
+
+
+def validate_org_id(c_key: str, c_val: str, keyz: dict, logg: ConfigurationCheckResult):
+    """Validate the CrowdStrike org UUID format."""
+    if c_key == "crowdstrike_org_uuid":
+        keyz[c_key] = invalid(logg, c_val) if not valid_uuid(c_val) else True
+
+
+def validate_galaxies_mapping(c_key: str, c_val: str, keyz: dict, logg: ConfigurationCheckResult):
+    """Validate the galaxy mapping file's existence."""
+    if c_key == "galaxies_map_file":
+        keyz[c_key] = warning(logg,
+            "WARNING: Specified galaxy mapping file not found"
+            ) if not os.path.exists(c_val) else True
+
+
+def validate_api_limits(c_key: str, c_val: str, keyz: dict, logg: ConfigurationCheckResult):
+    """Validate parameters relating to API limits."""
+    if c_key == "api_request_max":
+        keyz[c_key] = invalid(logg, c_val) if (5000 < int(c_val) or int(c_val) < 0) else True
+
+
+def validate_max_threads(c_key: str, c_val: str, keyz: dict, logg: ConfigurationCheckResult):
+    """Validate the max_threads parameter."""
+    if c_key == "max_threads":
+        hit = False
+        if not c_val:
+            c_val = 0
+        if int(c_val) < 0:
+            hit = invalid(logg, c_val)
+        if int(c_val) > 64:
+            hit = warning(logg, "WARNING: Potentially dangerous thread count specified")
+        if hit:
+            keyz[c_key] = hit
+
+
+def validate_login(auth: dict, logg: ConfigurationCheckResult):
+    """Validate that authentication generates a valid bearer token."""
+    auth_check = Intel(creds=auth["creds"], base_url=auth["base_url"])
+    if auth_check.token_status != 201:
+        logg.extra = {"key": "authentication"}
+        failure(logg, "CRITICAL: Invalid API credentials provided")
+    auth = None
+
+
+def show_debug_detail(c_key: str, c_val: str, _: dict, logg: ConfigurationCheckResult):
+    """Show parameter value debug output."""
+    dbg = c_val if c_key not in REDACTED else 'value redacted, check config file'
+    logg.put(f"DEBUG: {dbg if c_val else 'value not specified'}")
+
+
+def invalid(log_device: ConfigurationCheckResult, c_value: str):
+    """Process invalid parameter value alerts."""
+    return invalid_value(log_device, c_value)
+
+
+def validate_config(config_file: str = None, debugging: bool = False, no_banner: bool = False):
     """Review the configuration contents for errors and report the results."""
-    config = read_config_file(config_file)
     out = ConfigurationCheckResult(config_logging(debugging))
+    out.extra = {"key": ''}
+    if not no_banner:
+        for line in CONFIG_BANNER.split("\n"):
+            print(f"[{cur_time()}] INFO     config_check  {line}")
+    else:
+        print(f"[{cur_time()}] INFO     config_check  CHECK CONFIG")
+    config = read_config_file(config_file)
     keys = generate_primer()
-    test_creds = {"client_id": "Not set", "client_secret": "Not set", "base_url": "auto"}
+    auth_info = {"creds": {"client_id": "Not set", "client_secret": "Not set"}, "base_url": "auto"}
     for sect in config.sections() if config.sections() else not_found():
         for key in config[sect]:
             out.extra = {"key": key, "section": sect}
             val = config[sect].get(key)
-            invalid = lambda: invalid_value(out, key, val)
-            fail = lambda m: failure(out, msg_str=f"{keys[key]}: {m}")
-            alert = lambda m: warning(out, msg_str=f"{keys[key]}: {m}")
-            dbg = val if key not in REDACTED else 'value redacted, check config file'
-            out.put(f"DEBUG: {dbg}")
-            if key == "client_id":  # 32 chars
-                test_creds[key] = val
-                keys[key] = fail(
-                    "CrowdStrike client ID does not appear to be in the correct format"
-                    ) if len(val) != 32 else True
-            if key == "client_secret":  # 40 chars
-                test_creds[key] = val
-                keys[key] = fail(
-                    "CrowdStrike client secret does not appear to be in the correct format"
-                    ) if len(val) != 40 else True
-            if key == "misp_auth_key":  # x chars
-                keys[key] = fail(
-                    "MISP authorization key does not appear to be in the correct format"
-                ) if len(val) != 40 else True
-            if key == "crowdstrike_url":
-                test_creds[key] = val
-                keys[key] = alert(
-                    "Non-standard CrowdStrike URL specified"
-                    ) if not valid_base(val) else True
-            if key == "api_request_max":
-                keys[key] = invalid() if (5000 < int(val) or int(val) < 0) else True
-            if key == "api_enable_ssl":
-                keys[key] = alert(
-                    "SSL encryption is disabled for CrowdStrike API requests"
-                    ) if "f" in val.lower() else True
-            if key == "misp_enable_ssl":
-                keys[key] = alert(
-                    "SSL encryption is disabled for MISP API requests"
-                    ) if "f" in val.lower() else True
-            if key == "init_reports_days_before":
-                keys[key] = invalid() if (0 > int(val) or int(val) > 365) else True
-            if key == "init_indicators_minutes_before":
-                keys[key] = invalid() if (0 > int(val) or int(val) > 20220) else True
-            if key == "init_actors_days_before":
-                keys[key] = invalid() if (0 > int(val) or int(val) > 730) else True
-            if key in BOOL_KEYS:
-                keys[key] = invalid() if not bool_str(val) else True
-            if key == "galaxies_map_file":
-                keys[key] = alert(
-                    "Specified galaxy mapping file not found"
-                    ) if not os.path.exists(val) else True
-            if key == "crowdstrike_org_uuid":
-                keys[key] = invalid() if not valid_uuid(val) else True
-            if key == "max_threads":
-                hit = False
-                if not val:
-                    hit = invalid()
-                    val = 0
-                if int(val) > 64:
-                    hit = alert("Potentially dangerous thread count specified")
-                if hit:
-                    keys[key] = hit
+            vals = [key, val, keys, out]
+            show_debug_detail(*vals)
+            validate_crowdstrike_creds(*vals, auth_info)
+            validate_misp_creds(*vals)
+            validate_ssl(*vals)
+            validate_api_limits(*vals)
+            validate_start_times(*vals)
+            validate_booleans(*vals)
+            validate_galaxies_mapping(*vals)
+            validate_org_id(*vals)
+            validate_max_threads(*vals)
 
-    auth_check = Intel(creds=test_creds)
-    if auth_check.token_status != 201:
-        out.extra = {"key": "authentication"}
-        failure(out, "CRITICAL: Invalid API credentials provided")
-
+    validate_login(auth_info, out)
     check_for_missing(out, keys)
 
     return is_valid_config(out)
