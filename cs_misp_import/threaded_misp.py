52075792506f1cbdbe31d0ef774ac83bd79e30e3
Joshua Hiller <joshua.hiller@crowdstrike.com>
2022-10-24 01:14:03 -0400

    v0.6.4 - Log adjustments and additional overrides

--- cs_misp_import/threaded_misp.py
+++ cs_misp_import/threaded_misp.py
@@ -3,9 +3,12 @@ import requests
 import time
 import os
 
+from .misp_safe_check_response import safe_check_response
 try:
     import pymisp
     pymisp.api.everything_broken = {"key": ""}
+    pymisp.api.PyMISP._check_response = safe_check_response
+
     from pymisp import ExpandedPyMISP, PyMISPError
 
 except ImportError as no_pymisp:
@@ -24,16 +27,59 @@ class MISP(ExpandedPyMISP):
         kwargs.pop("max_threads")
         super().__init__(*args, **kwargs)
         self._PyMISP__session.mount('https://', requests.adapters.HTTPAdapter(pool_connections=int(self.thread_count), pool_maxsize=int(self.thread_count)*2))
-        
+        self.deleted_attribute_count = 0        
         self.deleted_event_count = 0
         self.deleted_tag_count = 0
+        self.added_sighting_count = 0
+        self.updated_event_count = 0
+
 
     def delete_event(self, *args, **kwargs):
         if self.deleted_event_count % 50 == 0 and self.deleted_event_count:
-            self.log.info("%i events deleted", self.deleted_event_count)
+            self.log.info("%i events deleted", self.deleted_event_count, extra={"key": ""})
         result = self._retry(super().delete_event, *args, **kwargs)
         if "errors" not in result:
             self.deleted_event_count += 1
+    
+
+    def add_sighting(self, *args, **kwargs):
+        thread_lock = kwargs.get("lock", None)
+        if thread_lock:
+            kwargs.pop("lock")
+        result = self._retry(super().add_sighting, *args, **kwargs)
+        if "errors" not in result:
+            if thread_lock:
+                with thread_lock:
+                    if self.added_sighting_count % 50 == 0 and self.added_sighting_count:
+                        self.log.info("%i sightings added", self.added_sighting_count)
+                    self.added_sighting_count += 1
+            else:
+                if self.added_sighting_count % 50 == 0 and self.added_sighting_count:
+                    self.log.info("%i sightings added", self.added_sighting_count)
+                self.added_sighting_count += 1
+
+
+    #def update_event(self, *args, **kwargs):
+        # thread_lock = kwargs.get("lock", None)
+        # kwargs.pop("lock")
+        # if self.updated_event_count % 50 == 0 and self.updated_event_count:
+        #     self.log.info("%i events updated", self.updated_event_count)
+        # result = self._retry(super().update_event, *args, **kwargs)
+        #self._retry(super().update_event, *args, **kwargs)
+        # if "errors" not in result:
+        #     if thread_lock:
+        #         with thread_lock:
+        #             self.updated_event_count += 1
+        #     else:
+        #         self.updated_event_count += 1
+
+    def delete_attribute(self, *args, **kwargs):
+        if self.deleted_attribute_count % 50 == 0 and self.deleted_attribute_count:
+            self.log.info("%i attributes deleted", self.deleted_attribute_count, extra={"key": ""})
+        result = self._retry(super().delete_attribute, *args, **kwargs)
+        if result:
+            if "errors" not in result:
+                self.deleted_attribute_count += 1
 
     def get_cs_tags(self):
         return self.search_tags("CrowdStrike:%")
@@ -43,8 +89,8 @@ class MISP(ExpandedPyMISP):
         #for tag in kwargstags:
         tag = args[0]
         if tag:
-            if self.deleted_tag_count % 50 == 0 and self.deleted_tag_count:
-                self.log.info("%i tags deleted", self.deleted_tag_count)
+            if not self.deleted_tag_count % 50 and self.deleted_tag_count:
+                self.log.info("%i tags deleted", self.deleted_tag_count, extra={"key": ""})
             result = self._retry(self.delete_tag, tag, **kwargs)
             if "errors" not in result:
                 self.deleted_tag_count += 1
@@ -71,15 +117,27 @@ class MISP(ExpandedPyMISP):
 
                 if i + 1 < self.MAX_RETRIES:
                     timeout = 0.3 * 2 ** i
-                    self.log.warning('Caught an error from the MISP server: %s. Re-trying the request in %.2f seconds', response['errors'], timeout)
+                    #self.log.warning('Caught an error from the MISP server. (T⌓T)', extra={"key": ""})
+                    self.log.warning('%s', dict(response['errors'])["message"], extra={"key": ""})
+                    self.log.warning('Retrying request in %.2f seconds. (T⌓T)', timeout, extra={"key": ""})
                     time.sleep(timeout)
                 else:
                     raise PyMISPError("MISP Error: {}".format(response['errors']))
             except Exception as e:
                 if i + 1 < self.MAX_RETRIES:
                     timeout = 0.3 * 2 ** i
-                    self.log.warning('Caught an error from the MISP server. Re-trying the request in %.2f seconds', timeout)
+                    #self.log.warning('Caught an error from the MISP server. ¯\_(ツ)_/¯', extra={"key": ""})
+                    self.log.warning('%s', str(e), extra={"key": ""})
+                    self.log.warning('Retrying request in %.2f seconds. ¯\_(ツ)_/¯', timeout, extra={"key": ""})
                     time.sleep(timeout)
                 else:
-                    self.log.exception('Caught an error from the MISP server. Exceeded number of retries.')
-                    raise e
+                    self.log.error('Unresolvable error received from the MISP server.', extra={"key": ""})
+                    try:
+                        self.log.error('%s', str(e.message), extra={"key": ""})
+                    except Exception as _:
+                        self.log.error("%s", str(e), extra={"key": ""})
+                    #self.log.exception("%s", dict(response['errors'])["message"], extra={"key": ""})
+                    self.log.error("Exceeded number of retries. (╯°□°）╯︵ ┻━┻", extra={"key": ""})
+                    #raise e
+
+
