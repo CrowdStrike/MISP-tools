4c1696030e1120057e06e437fe6c39717b9f3bb9
Joshua Hiller <joshua.hiller@crowdstrike.com>
2022-10-24 01:10:13 -0400

    v0.6.4 - Indicator Type enumerator

---
+++ cs_misp_import/indicator_type.py
@@ -0,0 +1,30 @@
+from enum import Enum
+
+class IndicatorType(Enum):
+    """CrowdStrike indicator type enumerator."""
+
+    HASH_MD5 = "MD5 hashes"
+    HASH_SHA256 = "SHA256 hashes"
+    HASH_SHA1 = "SHA1 hashes"
+    HASH_IMPHASH = "IMP hashes"
+    FILE_NAME = "File names"
+    FILE_PATH = "File directory paths"
+    URL = "Web addresses"
+    MUTEX_NAME = "Mutexes"
+    BITCOIN_ADDRESS = "BTC addresses"
+    COIN_ADDRESS = "BIC addresses"
+    EMAIL_ADDRESS = "Email addresses"
+    EMAIL_SUBJECT = "Email subjects"
+    REGISTRY = "Registry key locations"
+    DEVICE_NAME = "Device host names"
+    DOMAIN = "Web domains"
+    CAMPAIGN_ID = "Campaign IDs"
+    IP_ADDRESS = "IP addresses"
+    SERVICE_NAME = "Service names"
+    USER_AGENT = "User-Agent strings"
+    PORT = "TCP ports"
+    PASSWORD = "Password credentials"  # nosec  # unavoidable bandit false positive
+    USERNAME = "Credential user names"
+    X509_SERIAL = "Certificate serial numbers"
+    X509_SUBJECT = "Certificate subjects"
+    
